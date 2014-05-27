5edb11d0db69390ed663de0990c32dcb3efef636
Byron Walker-Mills <byronswfc@gmail.com>
2014-05-27 08:41:45 +0100

    add production database file

--- app/config/production/database.php
+++ app/config/production/database.php
@@ -6,10 +6,10 @@ return [
 
     'mysql' => [
         'driver'    => 'mysql',
-        'host'      => $_SERVER['DB1_HOST'],
-        'database'  => $_SERVER['DB1_NAME'],
-        'username'  => $_SERVER['DB1_USER'],
-        'password'  => $_SERVER['DB1_PASS'],
+        'host'      => $_SERVER['DB2_HOST'],
+        'database'  => $_SERVER['DB2_NAME'],
+        'username'  => $_SERVER['DB2_USER'],
+        'password'  => $_SERVER['DB2_PASS'],
         'charset'   => 'utf8',
         'collation' => 'utf8_unicode_ci',
         'prefix'    => ''
